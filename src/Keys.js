98058a2fe67c73b86da57cd7f653f49bc0e5ecbb
amine reda <amine.reda037@gmail.com>
2020-08-21 12:23:12 +0100

    add google-clone project

---
+++ src/Keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = "AIzaSyBL3-xn4OxPOo72flAj3DfO8dH6uIndljY";
+export default API_KEY;
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import API_KEY from "./Keys";
+
+const CONTEXT_KEY = "a46e322d6a604fd88";
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+  return { data };
+};
+
+export default useGoogleSearch;
